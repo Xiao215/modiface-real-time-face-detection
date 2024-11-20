15ebc4fe0bec9e595a3b380446108f4a18765ee0
Thomas <tomnguyenvn63@gmail.com>
2024-11-19 22:28:07 -0500

    Added Linear Block

--- test/src/main.rs
+++ test/src/main.rs
@@ -18,10 +18,14 @@ fn main() {
     let flattened_tensor = flatten.forward(tensor);
     println!("Flattened Tensor Shape: {:?}", flattened_tensor.dims());
 
-    //let convblock = mobilefacenet::ConvBlock::new([3, 64],[3, 3],&device);
-
     let convblock: mobilefacenet::ConvBlock<Backend> = mobilefacenet::ConvBlock::new([3, 64],[3, 3],[0,0], [1,1] ,1, &device);
     let tensor_2 = Tensor::<Backend, 4>::zeros([2, 3, 4, 5], &device);
     let conv_tensor  = convblock.forward(tensor_2);
     println!("Flattened Tensor Shape: {:?}", conv_tensor.dims());
+
+
+    let linblock: mobilefacenet::LinearBlock<Backend> = mobilefacenet::LinearBlock::new([3, 64],[1, 1],[0,0], [1,1] ,1, &device);
+    let tensor_3 = Tensor::<Backend, 4>::zeros([2, 3, 4, 5], &device);
+    let lin_tensor = linblock.forward(tensor_3);
+    println!("Flattened Tensor Shape: {:?}", lin_tensor.dims());
 }
--- test/src/mobilefacenet.rs
+++ test/src/mobilefacenet.rs
@@ -60,3 +60,26 @@ pub struct LinearBlock<B: Backend> {
 }
 
 
+impl<B: Backend> LinearBlock<B>{
+    pub fn new(channels: [usize; 2], kernel_size: [usize; 2], padding: [usize;2], stride: [usize; 2], groups: usize, device: &B::Device) -> Self {
+        let conv = nn::conv::Conv2dConfig::new(channels, kernel_size)
+            .with_padding(PaddingConfig2d::Explicit(padding[0], padding[1]))
+            .with_groups(groups)
+            .with_stride(stride)
+            .init(device);
+
+        let norm = nn::BatchNormConfig::new(channels[1]).init(device);
+
+        Self {
+            conv,
+            norm,
+        }
+    }
+
+    pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
+        let x = self.conv.forward(input);
+        let x = self.norm.forward(x);
+        x
+    }
+}
+
