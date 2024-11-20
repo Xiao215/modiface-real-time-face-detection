857422b8b45e21d02611796fa835c387b7dd86da
Thomas <tomnguyenvn63@gmail.com>
2024-11-19 23:20:22 -0500

    Commit

--- test/src/main.rs
+++ test/src/main.rs
@@ -18,14 +18,25 @@ fn main() {
     let flattened_tensor = flatten.forward(tensor);
     println!("Flattened Tensor Shape: {:?}", flattened_tensor.dims());
 
-    let convblock: mobilefacenet::ConvBlock<Backend> = mobilefacenet::ConvBlock::new([3, 64],[3, 3],[0,0], [1,1] ,1, &device);
+    // Input channels: 1
+    // Output Channels: 64
+    // Kernel Size: [1,1]
+    // Stride: [1,1]
+    // padding: [0,0]
+    // Groups: 1
+    let convblock: mobilefacenet::ConvBlock<Backend> = mobilefacenet::ConvBlock::new(3, 64, [1,1], [1,1], [0,0], 1, &device);
     let tensor_2 = Tensor::<Backend, 4>::zeros([2, 3, 4, 5], &device);
     let conv_tensor  = convblock.forward(tensor_2);
-    println!("Flattened Tensor Shape: {:?}", conv_tensor.dims());
+    println!("ConvBlock Tensor Shape: {:?}", conv_tensor.dims());
 
 
-    let linblock: mobilefacenet::LinearBlock<Backend> = mobilefacenet::LinearBlock::new([3, 64],[1, 1],[0,0], [1,1] ,1, &device);
+    let linblock: mobilefacenet::LinearBlock<Backend> = mobilefacenet::LinearBlock::new(3, 64, [1,1], [1,1], [0,0], 1, &device);
     let tensor_3 = Tensor::<Backend, 4>::zeros([2, 3, 4, 5], &device);
     let lin_tensor = linblock.forward(tensor_3);
-    println!("Flattened Tensor Shape: {:?}", lin_tensor.dims());
+    println!("Linear Block Tensor Shape: {:?}", lin_tensor.dims());
+
+    let depthwise: mobilefacenet::DepthWise<Backend> = mobilefacenet::DepthWise::new(3,64, false, [3,3],[2,2], [1,1], 1, &device);
+    let tensor_4 = Tensor::<Backend, 4>::zeros([2, 3, 4, 5], &device);
+    let depwise_tensor = depthwise.forward(tensor_4);
+    println!("Depthwise Block Tensor Shape: {:?}", depwise_tensor.dims());
 }
--- test/src/mobilefacenet.rs
+++ test/src/mobilefacenet.rs
@@ -21,6 +21,7 @@ impl Flatten {
     }
 }
 
+
 #[derive(Module, Debug)]
 pub struct ConvBlock<B: Backend> {
     conv: nn::conv::Conv2d<B>,
@@ -29,13 +30,13 @@ pub struct ConvBlock<B: Backend> {
 }
 
 impl<B: Backend> ConvBlock<B> {
-    pub fn new(channels: [usize; 2], kernel_size: [usize; 2], padding: [usize;2] ,stride: [usize; 2], groups: usize , device: &B::Device) -> Self {
-        let conv = nn::conv::Conv2dConfig::new(channels, kernel_size)
+    pub fn new(in_c: usize, out_c:usize, kernel: [usize; 2], stride: [usize; 2], padding: [usize; 2], groups: usize, device: &B::Device) -> Self {
+        let conv = nn::conv::Conv2dConfig::new([in_c, out_c], kernel)
             .with_padding(PaddingConfig2d::Explicit(padding[0], padding[1]))
             .with_groups(groups)
             .with_stride(stride)
             .init(device);
-        let norm = nn::BatchNormConfig::new(channels[1]).init(device);
+        let norm = nn::BatchNormConfig::new(out_c).init(device);
         let activation = nn::Relu::new();
 
         Self {
@@ -53,22 +54,22 @@ impl<B: Backend> ConvBlock<B> {
     }
 }
 
+
 #[derive(Module, Debug)]
 pub struct LinearBlock<B: Backend> {
     conv: nn::conv::Conv2d<B>,
     norm: BatchNorm<B, 2>,
 }
 
-
 impl<B: Backend> LinearBlock<B>{
-    pub fn new(channels: [usize; 2], kernel_size: [usize; 2], padding: [usize;2], stride: [usize; 2], groups: usize, device: &B::Device) -> Self {
-        let conv = nn::conv::Conv2dConfig::new(channels, kernel_size)
+    pub fn new(in_c: usize, out_c:usize, kernel: [usize; 2], stride: [usize; 2], padding: [usize; 2], groups: usize, device: &B::Device) -> Self {
+        let conv = nn::conv::Conv2dConfig::new([in_c, out_c], kernel)
             .with_padding(PaddingConfig2d::Explicit(padding[0], padding[1]))
             .with_groups(groups)
             .with_stride(stride)
             .init(device);
 
-        let norm = nn::BatchNormConfig::new(channels[1]).init(device);
+        let norm = nn::BatchNormConfig::new(out_c).init(device);
 
         Self {
             conv,
@@ -83,3 +84,32 @@ impl<B: Backend> LinearBlock<B>{
     }
 }
 
+
+#[derive(Module, Debug)]
+pub struct DepthWise<B: Backend> {
+    conv: ConvBlock<B>,
+    conv_dw: ConvBlock<B>,
+    project: LinearBlock<B>,
+}
+
+impl<B: Backend> DepthWise<B> {
+    pub fn new(input_c: usize, output_c: usize, residual: bool, kernel: [usize;2], stride: [usize;2], padding: [usize;2], groups: usize, device: &B::Device) -> Self {
+        let conv = ConvBlock::new(input_c, groups, [1,1], [1,1], [0,0], 1, device);
+        let conv_dw = ConvBlock::new(groups, groups, kernel, stride, padding, groups, device);
+        let project = LinearBlock::new(groups, output_c, [1,1], [1,1], [0,0], 1, device);
+
+        Self {
+            conv,
+            conv_dw,
+            project,
+        }
+    }
+
+    pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
+
+        let x = self.conv.forward(input);
+        let x = self.conv_dw.forward(x);
+        let x = self.project.forward(x);
+        x
+    }
+}
