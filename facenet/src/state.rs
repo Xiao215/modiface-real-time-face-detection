16c18d5e4aa9dcdc1c479f57ac16d718e3d8cbe3
mwangggg <mwangg02@gmail.com>
2024-12-01 18:15:33 -0500

    separate facenet

---
+++ facenet/src/state.rs
@@ -0,0 +1,29 @@
+use crate::mobilefacenet::MobileFaceNet;
+use burn::{
+    module::Module,
+    record::{BinBytesRecorder, FullPrecisionSettings, Recorder},
+};
+
+#[cfg(feature = "wgpu")]
+use burn::backend::wgpu::{init_async, AutoGraphicsApi, Wgpu, WgpuDevice};
+
+#[cfg(feature = "wgpu")]
+pub type Backend = Wgpu<f32, i32>;
+
+#[cfg(all(feature = "ndarray", not(feature = "wgpu")))]
+pub type Backend = burn::backend::ndarray::NdArray<f32>;
+
+static STATE_ENCODED: &[u8] = include_bytes!("../mobilefacenet.bin");
+
+/// Builds and loads trained parameters into the model.
+pub async fn build_and_load_model() -> MobileFaceNet<Backend> {
+    #[cfg(feature = "wgpu")]
+    init_async::<AutoGraphicsApi>(&WgpuDevice::default(), Default::default()).await;
+
+    let model: MobileFaceNet<Backend> = MobileFaceNet::new(512, "GDC", &Default::default());
+    let record = BinBytesRecorder::<FullPrecisionSettings>::default()
+        .load(STATE_ENCODED.to_vec(), &Default::default())
+        .expect("Failed to decode state");
+
+    model.load_record(record)
+}
